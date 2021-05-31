d7ca3ab5918fcf125050737a0418dab22c476e0b
Jin Lou <jinlou@bu.edu>
2021-05-31 10:09:52 -0400

    Fix input id by making each id unique.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import classes from './MealItemForm.module.css';
-import Input from '../../UI/Input';
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{id:'amount', type:'number', min:'1', max:'5', step:'1', defaultValue:'1'}}></Input>
-        <button>+ Add</button>
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          // create a unique id 
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      ></Input>
+      <button>+ Add</button>
     </form>
+  );
 };
 
 export default MealItemForm;
